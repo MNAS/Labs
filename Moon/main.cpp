48319c9b1fa2be2b98d625fdb813e4f36551b16a
Nick Matvyeyev <mnasoft@gmail.com>
2013-01-18 14:47:06 +0200

    mani.cpp was changed

--- Moon/main.cpp
+++ Moon/main.cpp
@@ -6,11 +6,45 @@
 
 int main() 
 {
-  Point a;
-    Romb Aa;
-     Aa.input();
-     Aa.output();
-     printf("%f\n",Aa.perimetr());
-     printf("%f\n",Aa.area());
+    Romb Rb;
+     Rb.input();
+     Rb.output();
+     printf("%f\n",Rb.perimetr());
+     printf("%f\n",Rb.area());
+     
+  Moon Mn;
+    Mn.input();
+    Mn.output();
+    printf("%f\n",Mn.perimetr());
+    printf("%f\n",Mn.area());
     return 0;
 }
+
+
+
+ /*   Point A;
+    Point B(5,6);
+    Point C;
+    Arc a;
+    Arc b(10,20,30);
+    Arc c(b);
+    Arc d(B,15);
+    
+    a.input();
+    
+    a.output(); printf("\n");
+    b.output(); printf("\n");
+    c.output(); printf("\n");
+    d.output(); printf("\n");
+    
+   
+    
+    
+    
+    
+    A.output();
+    B.output();
+    C.input();
+    C.output();
+    printf("Distance=%f\n",B.distance(C));
+*/  
