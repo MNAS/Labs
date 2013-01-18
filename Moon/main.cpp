f7303e0613a382da016807c19d7dc398747d0bd2
Nick Matvyeyev <mnasoft@gmail.com>
2013-01-18 14:03:04 +0200

    Добавлена процедура для тестирования  класса Romb

--- Moon/main.cpp
+++ Moon/main.cpp
@@ -1,40 +1,16 @@
 #include <stdio.h> 
 #include "Point.h" 
+#include "Romb.h"
 #include "Arc.h"
 #include "Moon.h"
 
 int main() 
 {
- /*   Point A;
-    Point B(5,6);
-    Point C;
-    Arc a;
-    Arc b(10,20,30);
-    Arc c(b);
-    Arc d(B,15);
-    
-    a.input();
-    
-    a.output(); printf("\n");
-    b.output(); printf("\n");
-    c.output(); printf("\n");
-    d.output(); printf("\n");
-    
-   
-    
-    
-    
-    
-    A.output();
-    B.output();
-    C.input();
-    C.output();
-    printf("Distance=%f\n",B.distance(C));
-*/  
-    Moon Aa;
-    Aa.input();
-    Aa.output();
-    printf("%f\n",Aa.perimetr());
-    printf("%f\n",Aa.area());
+  Point a;
+    Romb Aa;
+     Aa.input();
+     Aa.output();
+     printf("%f\n",Aa.perimetr());
+     printf("%f\n",Aa.area());
     return 0;
 }
