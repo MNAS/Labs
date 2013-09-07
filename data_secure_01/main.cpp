636450ac849acc79964eb3d7b1779d1169550e9c
Nick Matvyeyev <mnasoft@gmail.com>
2013-09-07 15:27:04 +0300

    renamed:    ../qt4/main.cpp -> main.cpp

--- data_secure_01/main.cpp
+++ data_secure_01/main.cpp
@@ -1,71 +1,78 @@
-// map::find
-#include <iostream>
+#include <QTextStream>
+#include <QFile>
+//#include <iostream>
 #include <map>
-#include <string>
-#include <fstream>
 
-int main(int argc, char *argv[])
+
+int main(int argc, char **argv)
 {
-    std::map<char, int> mymap;
-    std::map<char, double> map_fr;
-    std::map<char, std::string> map_color;
-    std::map<char, int>::iterator it;
-    std::map<char, double>::iterator i_map_fr;
-    char ch;
+    std::map<QChar, int> mymap;
+    std::map<QChar, double> map_fr;
+    std::map<QChar, QString> map_color;
+    std::map<QChar, int>::iterator it;
+    std::map<QChar, double>::iterator i_map_fr;
 
-    if (argc != 2) {
-        std::cout << "Usage:" << std::endl;
-        std::cout << "data_secure_01 file_name" << std::endl;
+    QTextStream cout(stdout);
+    cout.setCodec("UTF-8");
+    QChar ch;
+
+    if (argc != 2)
         return 1;
-    }
-    std::ifstream in(argv[1]);
-    if (!in.good())
-        return 2;
+    QFile in_f(argv[1]);
+    if (in_f.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QTextStream in(&in_f);
+        in.setCodec("UTF-8");
 
-    while (in.good()) {
-        in >> ch;
-        it = mymap.find(ch);
-        if (it ==  mymap.end())
-            mymap[ch] = 1;
-        else
-            it->second++;
+        while (!in.atEnd()) {
+            in >> ch;
+            it = mymap.find(ch);
+            if (it ==  mymap.end())
+                mymap[ch] = 1;
+            else
+                it->second++;
+        }
     }
-    in.close();
+    in_f.close();
+
     int counter = 0;
-    for (std::map<char, int>::iterator i = mymap.begin(); i != mymap.end(); ++i) {
+    for (std::map<QChar, int>::iterator i = mymap.begin(); i != mymap.end(); ++i) {
         counter += i->second;
     }
 
-    for (std::map<char, int>::iterator i = mymap.begin(); i != mymap.end(); ++i) {
+    for (std::map<QChar, int>::iterator i = mymap.begin(); i != mymap.end(); ++i) {
         map_fr[i->first] = double(i->second) / double(counter);
-        if (map_fr[i->first] < 0.2)
+        if (map_fr[i->first] < 0.02)
             map_color[i->first] = "\e[0;31m";
-        else if (map_fr[i->first] < 0.4)
+        else if (map_fr[i->first] < 0.04)
             map_color[i->first] = "\e[0;32m";
-        else if (map_fr[i->first] < 0.6)
+        else if (map_fr[i->first] < 0.08)
             map_color[i->first] = "\e[0;33m";
-        else if (map_fr[i->first] < 0.8)
+        else if (map_fr[i->first] < 0.15)
             map_color[i->first] = "\e[0;34m";
         else if (map_fr[i->first] <= 1.0)
             map_color[i->first] = "\e[0;35m";
     }
 
 
-    for (std::map<char, int>::iterator i = mymap.begin(); i != mymap.end(); ++i) {
-        std::cout << i->first << " " << i->second << std::endl;
+    for (std::map<QChar, int>::iterator i = mymap.begin(); i != mymap.end(); ++i) {
+        cout << i->first << " " << i->second << "\n";
     }
 
-    for (std::map<char, double>::iterator i = map_fr.begin(); i != map_fr.end(); ++i) {
-        std::cout << i->first << " " << i->second << std::endl;
+    for (std::map<QChar, double>::iterator i = map_fr.begin(); i != map_fr.end(); ++i) {
+        cout << i->first << " " << i->second  << "\n";
     }
-    in.open(argv[1]);
 
-    if (!in.good())
-        return 3;
 
-    while (in.good()) {
-        in >> ch;
-        std::cout << map_color[ch] << ch << "\e[0m";
+    if (in_f.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QTextStream in(&in_f);
+        in.setCodec("UTF-8");
+
+        while (!in.atEnd()) {
+            in >> ch;
+            cout << map_color[ch] << ch << "\e[0m";
+        }
     }
+    in_f.close();
+
     return 0;
 }
