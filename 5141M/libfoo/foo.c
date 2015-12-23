b4db89e2478f9d6e6f362348e10f9e9cd0b3950a
Nick Matvyeyev <mnasoft@gmail.com>
2015-12-23 23:16:41 +0200

    Создан Makefile

---
+++ 5141M/libfoo/foo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "foo.h"
+void foo(void)
+{
+ printf("Hello, I'm a shared library libfoo!\n");
+}
---
+++ 5141M/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "libfoo/foo.h"
+
+int main(int argc , char** argv)
+{
+  printf("COOOOOL\n");
+  foo();
+  return 0;
+}
